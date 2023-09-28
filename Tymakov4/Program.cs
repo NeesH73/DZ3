a937a1614a4838cb38804c298ece360b5974c916
smetanin73i@gmail.com <smetanin73i@gmail.com>
2023-09-28 15:43:29 +0300

    Добавьте файлы проекта.

---
+++ Tymakov4/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+
+namespace Tymakov4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упр 4.1: перевести число в день месяца");
+            Console.Write("Введите число[1-365]: ");
+            int day = int.Parse(Console.ReadLine());
+            DateTime date = new DateTime(1, 1, 1);
+            Console.WriteLine($"{day} соответствует: {date.AddDays(day - 1).ToString("dd MMMM")}");
+
+
+
+            Console.WriteLine("Упр 4.2: добавить проверку к упр. 4.1");
+            Console.Write("Введите число[1-365]: ");
+            int day1 = int.Parse(Console.ReadLine());
+            if (day1 < 1 || day1 > 365)
+            {
+                Console.WriteLine(" Вы ввели число меньше 1 или больше 365");
+            }
+            else
+            {
+                DateTime date1 = new DateTime(1, 1, 1);
+                Console.WriteLine($"{day1} соответствует: {date1.AddDays(day1 - 1).ToString("dd MMMM")}");
+            }
+
+
+
+            Console.WriteLine("ДЗ 4.1: сделать проверку на високосный год");
+            Console.Write("Введите число[1-365]: ");
+            int day2 = int.Parse(Console.ReadLine());
+            if (day2 < 1 || day2 > 365)
+            {
+                Console.WriteLine(" Вы ввели число меньше 1 или больше 365");
+            }
+            else
+            {
+                DateTime date2 = new DateTime(1, 1, 1);
+                Console.WriteLine($"{day2} соответствует: {date2.AddDays(day2 - 1).ToString("dd MMMM")}");
+            }
+            Console.Write("Введите год: ");
+            int year=int.Parse(Console.ReadLine());
+            if ((year%4==0 && year%100!=0) || (year%400==0))
+            {
+                Console.WriteLine($"{year} високосный");
+            }
+            else 
+            {
+                Console.WriteLine($"{year} не високосный");
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ zadachi/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+
+namespace zadachi
+{
+    internal class Program
+    {
+        enum cards
+        {
+            Шестерка = 6,
+            Семерка = 7,
+            Восьмерка = 8,
+            Девятка = 9,
+            Десятка = 10,
+            Валет = 11,
+            Дама = 12,
+            Король = 13,
+            Туз = 14
+
+        }
+        enum days
+        {
+            Понедельник = 1,
+            Вторник = 2,
+            Среда = 3,
+            Четверг = 4,
+            Пятница = 5,
+            Суббота = 6,
+            Воскресенье = 7
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 1: определить последовательность(возр.или нет)");
+            int[] mas = new int[10];
+            bool flag = true;
+            Random randomNumber = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                mas[i] = randomNumber.Next();
+                Console.WriteLine(mas[i] + " ");
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                if (mas[i] > mas[i + 1])
+                {
+                    Console.WriteLine("Последовательность не возр.");
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag)
+            {
+                Console.WriteLine("Последовательность возр.");
+            }
+
+
+            Console.WriteLine("Задание 2: определить карту по номеру");
+            try
+            {
+                if (!byte.TryParse(Console.ReadLine(), out byte n) || (n < 6 || n > 14))
+                {
+                    Console.WriteLine("Введите число от 6 до 14");
+                }
+                else
+                {
+                    Console.WriteLine((cards)n);
+                }
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+
+            Console.WriteLine("Задание 3: Кто вы");
+            switch (Console.ReadLine().ToLower())
+            {
+                case "jabroni":
+                    Console.WriteLine("Patron Tequila\n");
+                    break;
+                case "school counselor":
+                    Console.WriteLine("Anything with Alcohol\n");
+                    break;
+                case "programmer":
+                    Console.WriteLine("Hipster Craft Beer\n");
+                    break;
+                case "bike gang member":
+                    Console.WriteLine("Moonshine\n");
+                    break;
+                case "politician":
+                    Console.WriteLine("Your tax dollars\n");
+                    break;
+                case "rapper":
+                    Console.WriteLine("Cristal\n");
+                    break;
+                default:
+                    Console.WriteLine("Beer\n");
+                    break;
+            }
+
+
+            Console.WriteLine("Задание 4: Вывести день недели");
+            try
+            {
+                if (!byte.TryParse(Console.ReadLine(), out byte dw) || (dw < 1 || dw > 7))
+                {
+                    Console.WriteLine("Введите число от 1 до 7");
+                }
+                else
+                {
+                    Console.WriteLine((days)dw);
+                }
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+
+            Console.WriteLine("Задание 5: обойти массив и положить в сумку опред. предмет ");
+            string[]  Toys= { "Hello Kitty", "Barbie doll", "Bart", "Swer", "Cit", "Hello Kitty", "Canya", "Nero","Larl" ,"Barbie doll", "Nix" };
+            byte kol = 0;
+            foreach(string Toy in Toys)
+            {
+                if (Toy == "Barbie doll" || Toy == "Hello Kitty")
+                {
+                    kol++;
+                }
+            }
+            Console.WriteLine($"В сумке лежит {kol} кукол.");
+        }
+    }
+}
